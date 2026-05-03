842250babf61813d2e49eeb464ea606f03ff2343
Dzmitry <dzmitrij.l@tuta.io>
2026-05-03 15:03:16 +0400

    add license.

--- lib/insightnest/application.ex
+++ lib/insightnest/application.ex
@@ -1,3 +1,15 @@
+# InsightNest - A privacy-focused, community co-owned knowledge platform.
+# Copyright (C) 2026 Dzmitry Litmanovich
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
 defmodule Insightnest.Application do
   use Application
 
